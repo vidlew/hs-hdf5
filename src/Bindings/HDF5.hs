e632a57a429ab16d1c86d9a18eaf32f9e92f3d54
Picca Frédéric-Emmanuel <picca@debian.org>
2016-04-09 15:00:49 +0200

    hlint

--- src/Bindings/HDF5.hs
+++ src/Bindings/HDF5.hs
@@ -1,64 +1,30 @@
 module Bindings.HDF5
-    ( HId
-    , FromHId
-    , HAddr
-    , HSize
-    , HSSize
-    , Location
-    , IndexType(..)
+    ( IndexType(..)
     , IterOrder(..)
     , IH_Info(..)
-
-    , module Bindings.HDF5.Dataset
-    , module Bindings.HDF5.Dataspace
-    , module Bindings.HDF5.Datatype
-    , module Bindings.HDF5.File
-    , module Bindings.HDF5.Group
-    , module Bindings.HDF5.Link
-    , module Bindings.HDF5.Object
-    , module Bindings.HDF5.PropertyList
-
-    , module Bindings.HDF5.PropertyList.DAPL
-    , module Bindings.HDF5.PropertyList.DCPL
-    , module Bindings.HDF5.PropertyList.DXPL
-    , module Bindings.HDF5.PropertyList.FAPL
-    , module Bindings.HDF5.PropertyList.FCPL
-    , module Bindings.HDF5.PropertyList.FMPL
-    , module Bindings.HDF5.PropertyList.GAPL
-    , module Bindings.HDF5.PropertyList.GCPL
-    , module Bindings.HDF5.PropertyList.LAPL
-    , module Bindings.HDF5.PropertyList.LCPL
-    , module Bindings.HDF5.PropertyList.OCPL
-    , module Bindings.HDF5.PropertyList.OCPYPL
-    , module Bindings.HDF5.PropertyList.STRCPL
-    , module Bindings.HDF5.PropertyList.TAPL
-    , module Bindings.HDF5.PropertyList.TCPL
+    , module X
     ) where
 
-import Bindings.HDF5.Core
-import Bindings.HDF5.Dataset
-import Bindings.HDF5.Dataspace
-import Bindings.HDF5.Datatype
-import Bindings.HDF5.File
-import Bindings.HDF5.Group
-import Bindings.HDF5.Link
-import Bindings.HDF5.Object
-import Bindings.HDF5.PropertyList
-import Bindings.HDF5.PropertyList.DAPL
-import Bindings.HDF5.PropertyList.DCPL
-import Bindings.HDF5.PropertyList.DXPL
-import Bindings.HDF5.PropertyList.FAPL
-import Bindings.HDF5.PropertyList.FCPL
-import Bindings.HDF5.PropertyList.FMPL
-import Bindings.HDF5.PropertyList.GAPL
-import Bindings.HDF5.PropertyList.GCPL
-import Bindings.HDF5.PropertyList.LAPL
-import Bindings.HDF5.PropertyList.LCPL
-import Bindings.HDF5.PropertyList.OCPL
-import Bindings.HDF5.PropertyList.OCPYPL
-import Bindings.HDF5.PropertyList.STRCPL
-import Bindings.HDF5.PropertyList.TAPL
-import Bindings.HDF5.PropertyList.TCPL
+import Bindings.HDF5.Core as X
+import Bindings.HDF5.Dataset as X
+import Bindings.HDF5.Dataspace as X
+import Bindings.HDF5.Datatype as X
+import Bindings.HDF5.File as X
+import Bindings.HDF5.Group as X
+import Bindings.HDF5.Link as X
+import Bindings.HDF5.Object as X
+import Bindings.HDF5.PropertyList as X
+import Bindings.HDF5.PropertyList.DAPL as X
+import Bindings.HDF5.PropertyList.DCPL as X
+import Bindings.HDF5.PropertyList.DXPL as X
+import Bindings.HDF5.PropertyList.FAPL as X
+import Bindings.HDF5.PropertyList.FCPL as X
+import Bindings.HDF5.PropertyList.FMPL as X
+import Bindings.HDF5.PropertyList.GAPL as X
+import Bindings.HDF5.PropertyList.OCPYPL as X
+import Bindings.HDF5.PropertyList.STRCPL as X
+import Bindings.HDF5.PropertyList.TAPL as X
+import Bindings.HDF5.PropertyList.TCPL as X
 
 
 

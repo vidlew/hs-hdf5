7ad88e4e5c70f8f298ef70a313a9dd0c6fbec69b
Stephen Pascoe <Stephen.Pascoe@nanoporetech.com>
2017-09-27 17:36:54 +0100

    More H5 tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -6,6 +6,8 @@ import Control.Exception (bracket)
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Bindings.HDF5.File as F
+import qualified Data.Vector.Storable as SV
+
 
 main :: IO ()
 main = hspec $ do
@@ -24,6 +26,38 @@ main = hspec $ do
         return (fn, BS.pack path)
       fn `shouldBe` path
 
+    {- TODO : create a group and mount it there so we can test something about it
+    it "Allows mounting of one file in another" $ do
+      x <- withTestFile $ \path file1 -> let callback file2 = do
+                                               F.mountFile file1 (BS.pack "/") file2 Nothing
+                                               return 42
+                                         in withTestFile' callback
+      x `shouldBe` 42
+    -}
+
+    it "global object count is zero initially" $ do
+      count <- F.getFileObjCount Nothing True [F.All]
+      count `shouldBe` 0
+
+    it "test file has one file" $ do
+      count <- withTestFile' $ \file -> F.getFileObjCount (Just file) True [F.Files]
+      count `shouldBe` 1
+
+    it "test file has no groups" $ do
+      count <- withTestFile' $ \file -> F.getFileObjCount (Just file) True [F.Groups]
+      count `shouldBe` 0
+
+    it "test file has no datasets" $ do
+      count <- withTestFile' $ \file -> F.getFileObjCount (Just file) True [F.Datasets]
+      count `shouldBe` 0
+
+    it "test file has 1 open object" $ do
+      vec <- withTestFile' $ \file -> F.getOpenObjects (Just file)True [F.All]
+      SV.length vec `shouldBe` 1
+
+    it "test file has no free space" $ do
+      size <- withTestFile' $ \file -> F.getFileFreespace file
+      size `shouldBe` 0
 
 -- | Call a handle with a temporary HDF5 file
 withTestFile :: (FilePath -> F.File -> IO a) -> IO a

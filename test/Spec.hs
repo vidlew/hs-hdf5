2240953107843785f2278acbe5c7589ee6bdab01
Stephen Pascoe <Stephen.Pascoe@nanoporetech.com>
2017-09-27 16:39:21 +0100

    Close test files after tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,6 +2,7 @@ import Test.Hspec
 import Test.QuickCheck
 import System.IO.Temp
 import System.IO
+import Control.Exception (bracket)
 
 import qualified Data.ByteString.Char8 as BS
 import qualified Bindings.HDF5.File as F
@@ -11,7 +12,9 @@ main = hspec $ do
   describe "File interface" $ do
     it "isHDF5 returns True for the test file" $ do
       isHdf5 <- withTestFile $ \path file -> do
-        F.closeFile file
+        -- Flish file to disk
+        F.flushFile file F.Global
+        -- Return whether it is a valid HDF5 file
         F.isHDF5 (BS.pack path)
       isHdf5 `shouldBe` True
 
@@ -25,11 +28,12 @@ main = hspec $ do
 -- | Call a handle with a temporary HDF5 file
 withTestFile :: (FilePath -> F.File -> IO a) -> IO a
 withTestFile callback = withSystemTempFile "hs-hdf5.hdf" callback2 where
-  callback2 path handle = do
-    hClose handle
-    file <- F.createFile (BS.pack path) [F.Truncate] Nothing Nothing
-    -- TODO : Close file before returning
-    callback path file
+  callback2 path handle = bracket aquire release run where
+    aquire = do
+      hClose handle
+      F.createFile (BS.pack path) [F.Truncate] Nothing Nothing
+    run file = do callback path file
+    release file = do F.closeFile file
 
 withTestFile' :: (F.File -> IO a) -> IO a
 withTestFile' callback = withTestFile (\path file -> callback file)
